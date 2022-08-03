6a597b70851639cd9dd651947d6e190de450bd4d
LeonardoSantos16 <leodsmf@gmail.com>
2022-08-03 01:21:44 -0300

    primeiro-commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BookCartComponent } from './components/book-cart/book-cart.component';
+import { FiltersComponent } from './components/book-cart/filters/filters.component';
+import { ProductListComponent } from './components/book-cart/product-list/product-list.component';
+import { ProductItemComponent } from './components/book-cart/product-list/product-item/product-item.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { NavComponent } from './components/shared/nav/nav.component';
+import { BooksService } from './components/book-cart/product-list/product-list.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavComponent,
+    BookCartComponent,
+    ProductListComponent,
+    FiltersComponent,
+    ProductItemComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [BooksService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
